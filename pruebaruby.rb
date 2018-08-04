2392dd6f4408775afa7d3a4ff6ff903e7830cc7f
evelyn <astudillobarra.evelyn@gmail.com>
2018-08-04 11:59:02 -0400

    final commit

--- pruebaruby.rb
+++ pruebaruby.rb
@@ -4,28 +4,30 @@ data = file.readlines.map(&:chomp)
 file.close
 
   def promedio(data)
-    file = File.new('average.txt', 'w')
-    data.each do |line|
-    ls = line.split(', ')
-       name = ls[0]
-       calif = ls[1..5].each.map(&:to_i)
-       suma = 0
-         calif.each do |num|
-           suma += num
+  file = File.new('average.txt', 'w')
+  data.each do |line|
+  ls = line.split(', ')
+  name = ls[0]
+  calif = ls[1..5].each.map(&:to_i)
+  suma = 0
+  calif.each do |num|
+  suma += num
   end
-   average = suma.to_f / calif.size
-   file.puts "El promedio de #{name} es #{average}"
-      end
-file.close
-puts "Se ha creado tu archivo"
-    end
+  p average = suma.to_f / calif.size
+  file.puts "El promedio de #{name} es #{average}"
+  end
+  file.close
+  puts "Se ha creado tu archivo"
+  end
+  promedio(data)
+
 
   def inasistencias(data)
     data.each do |line|
     ls = line.split(', ')
        name = ls[0]
        calif = ls[1..5].each.map(&:to_i)
-       p leave = calif.count(0)
+        leave = calif.count(0)
        if leave >= 1
          puts "Alumne #{name} tiene #{leave} inasistencias"
        end
@@ -46,17 +48,17 @@ puts "Se ha creado tu archivo"
   if average >= min_approved
     puts "#{name} aprueba con nota #{average}"
   end
-  
+
   end
   return nil
 end
-aprobados(data)
 
-puts 'INGRESE SUS OPCIONES'
+
+puts 'Ingrese una Opción'
     opcion = 0
     while opcion != 4
 
-      puts "____________________________"
+      puts "****************************"
       puts '1 = Promedio de cada alumno'
       puts '2 = Inasistencias Totales'
       puts '3 = Alumnos Aprobados'
